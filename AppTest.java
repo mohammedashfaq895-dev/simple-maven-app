af272ccf39e8bd03c6bf52a3ba9935cc05e89067
mohammedashfaq895-dev <mohammedashfaq895@gmail.com>
2026-04-04 21:11:57 +0530

    Update AppTest.java

--- AppTest.java
+++ AppTest.java
@@ -1,1 +1,20 @@
+package com.example;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testGrades() {
+        App app = new App();
+
+        assertEquals("Grade S", app.getGrade(95));
+        assertEquals("Grade A", app.getGrade(85));
+        assertEquals("Grade B", app.getGrade(75));
+        assertEquals("Grade C", app.getGrade(65));
+        assertEquals("Grade D", app.getGrade(55));
+        assertEquals("Grade E", app.getGrade(45));
+        assertEquals("Grade F", app.getGrade(30));
+    }
+}
